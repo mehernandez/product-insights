9912ccf1dca62e6dc3acb08ec4545661b4119368
Mario Hernandez <med.hernandez10@gmail.com>
2016-03-13 20:17:53 -0500

    Initial Commit

---
+++ Product Insights/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Product Insights
+//
+//  Created by Mario Hernandez on 3/13/16.
+//  Copyright © 2016 Mario Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
